21d0d9d111740e4c8f6f277b188120be84f8fbda
yixin.huang <yixin.huang@monash.edu>
2025-01-17 17:11:10 +0800

    how to hcange greatings?

--- blog.config.js
+++ blog.config.js
@@ -24,7 +24,7 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，我是一个算法工程师,Hi，我是一个摄影师,Hi，我是一个投资学者,欢迎来到我的博客🎉',
+    'Hi, 我是一个算法工程师, 一个摄影师, 一个投资小白, 欢迎来到我的博客🎉',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
